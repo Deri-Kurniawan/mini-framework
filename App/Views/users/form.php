86a834c746cf93bf3772665c469136578a7991f7
Deri Kurniawan <deri.netuchi@gmail.com>
2022-01-06 16:42:42 +0700

    change dot path to slash & fix templating engine

--- App/Views/users/form.php
+++ App/Views/users/form.php
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
+<?= addComponent('layouts.components.header', $title) ?>
+<?= addComponent('layouts.components.navbar') ?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?= $title; ?></title>
-</head>
+<form action="<?= baseUrl('users/save'); ?>" method="post">
+  <input type="text" name="name" id="name" class="" value="" placeholder="">
+  <button>simpan</button>
+</form>
 
-<body>
-  <?= addComponent('layouts.components.navbar') ?>
-
-  <form action="<?= base_url('users.save'); ?>" method="post">
-    <input type="text" name="name" id="name" class="" value="" placeholder="">
-    <button>simpan</button>
-  </form>
-</body>
-
-</html>
+<?= addComponent('layouts.components.footer') ?>
